d8922b8917c92418e68f79778d607d6d94ed4174
Honey <yurjune@naver.com>
2021-09-19 18:26:38 +0900

    add sunday page

---
+++ front/components/ItemCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Box, Square, Image } from "@chakra-ui/react";
+
+const ItemCard = ({ post }) => {
+
+  return (
+    <>
+      <Square size="225px" mb="15px">
+        <Image src={post.Thumbnail.src}/>
+      </Square>
+      <Box
+        w="225px"
+        fontSize="20px"
+        fontWeight="bold"
+        textAlign="center"
+      >
+        {post.content}
+      </Box>
+    </>
+  );
+};
+
+export default ItemCard;
--- front/pages/movies/sunday.js
+++ front/pages/movies/sunday.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Head from 'next/head'
+import { Flex, Button } from '@chakra-ui/button';
 
 import AppLayout from '../../components/AppLayout';
 import ContentsBar from '../../components/ContentsBar';
 import ItemCard from '../../components/ItemCard';
 
+const dummyPost = {
+  id: 1,
+  content: "말씀으로 승리하라",
+  Thumbnail: {
+    src: "https://via.placeholder.com/300",
+  },
+  Images: {
+    src: "https://via.placeholder.com/300",
+  },
+  User: {
+    id: 1,
+  }
+}
 
 const Sunday = () => {
+  const [posts, setPosts] = useState([]);
+
+  const createButton = () => {
+    setPosts((prev) => [dummyPost, ...prev]);
+  }
+
   return (
     <>
       <Head>
@@ -15,7 +35,8 @@ const Sunday = () => {
       </Head>
       <AppLayout>
         <ContentsBar title="설교영상" btn1="전체" btn2="성경" btn3="주제"/>
-        <ItemCard></ItemCard>
+          {posts.map(post => <ItemCard post={post}></ItemCard>)}
+        <Button onClick={createButton}>등록</Button>
       </AppLayout>
     </>
   );
