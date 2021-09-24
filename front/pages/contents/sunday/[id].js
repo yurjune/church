e8010ae173e0bc5f3303a6eb2b09bfbaa9261548
Honey <yurjune@naver.com>
2021-09-24 17:51:21 +0900

    fix useFetch function

--- front/pages/contents/sunday/[id].js
+++ front/pages/contents/sunday/[id].js
@@ -25,12 +25,27 @@ const Content = () => {
   const category = encodeURIComponent("주일예배");
   const { id } = router.query;
 
+  const { data, error, isLoading } = useFetch(category, id);
+
+  // useEffect(() => {
+  //   console.log('정보: ', data);
+  // }, [data]);
+
+  if (isLoading) {
+    return <div>로딩 중...</div>
+  }
+
+  if (error) {
+    console.error(error);
+    return <div>게시글을 불러오는 중 문제가 발생했습니다.</div>
+  }
+
   return (
     <>
       <AppLayout>
         <Flex>
-          <Sidebar category={category} id={id}/>
-          <MainContent/>
+          <Sidebar content={data}/>
+          <MainContent content={data}/>
         </Flex>
       </AppLayout>
     </>
