60c971e1ff92d305f00d4c6192a96387ceee8ae5
Honey <yurjune@naver.com>
2021-09-30 19:59:04 +0900

    Update footer

--- front/components/Footer.js
+++ front/components/Footer.js
@@ -3,26 +3,23 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <>
+    <Box w="100%" h="180px" bgColor="charcole">
       <Flex
-        w="100%"
+        maxW="960px"
         h="180px"
-        bgColor="#282828"
-        justify="center"
-        align="center"
+        m="0 auto"
+        direction="column"
         color="grayLetter"
-        fontFamily="sans-serif"
         fontSize="12px"
+        px={{ base: "20px", sm: "30px", md: "40px", lg: "0" }}
+        py="30px"
+        justify={{ base: "center", md: "flex-start" }}
+        align={{ base: "center", md: "flex-start" }}
       >
-        <Box
-          w="80%"
-          h="120px"
-        >
-          <Box py="10px" mb="16px">© 2021 DIDIMDOL CHURCH. ALL RIGHT RESERVED</Box>
-          <Box>경기도 의왕시 보우상가</Box>
-        </Box>
+        <Box py="10px" mb="16px">© 2021 DIDIMDOL CHURCH. ALL RIGHT RESERVED</Box>
+        <Box>경기도 의왕시 보우상가</Box>
       </Flex>
-    </>
+    </Box>
   );
 };
 
