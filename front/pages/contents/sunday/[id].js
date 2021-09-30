84124bde7a89022f77d6a71f997e58d82463e425
Honey <yurjune@naver.com>
2021-10-01 04:02:42 +0900

    Add useFetch hook

--- front/pages/contents/sunday/[id].js
+++ front/pages/contents/sunday/[id].js
@@ -1,43 +1,25 @@
 import React, { useEffect } from 'react';
-import useSWR from 'swr';
-import axios from 'axios';
 import { useRouter } from 'next/router';
 import { Box, Flex } from '@chakra-ui/react';
 
 import AppLayout from '../../../components/AppLayout';
 import MovieCard from '../../../components/MovieCard';
 import MovieArticle from '../../../components/MovieArticle';
-
-export const useFetch = (category, id) => {
-  const fetcher = (url) => axios.get(url, { withCredentials: true }).then((result) => result.data);
-  const { data, error, mutate } = useSWR(
-    `http://localhost:3060/post?category=${category}&id=${id}`, fetcher,
-  );
-  return {
-    data,
-    error,
-    isLoading: !error && !data,
-  }
-}
+import useFetch from '../../../hooks/useFetch';
 
 const Content = () => {
   const router = useRouter();
   const category = encodeURIComponent("주일예배");
   const { id } = router.query;
-
-  const { data, error, isLoading } = useFetch(category, id);
-
-  // useEffect(() => {
-  //   console.log('정보: ', data);
-  // }, [data]);
+  const url = `/post?category=${category}&id=${id}`
+  const { data, error, isLoading } = useFetch(url);
 
   if (isLoading) {
-    return <div>로딩 중...</div>
+    return <div>로딩 중...</div>;
   }
-
   if (error) {
     console.error(error);
-    return <div>게시글을 불러오는 중 문제가 발생했습니다.</div>
+    return <div>게시글을 불러오는 중 문제가 발생했습니다.</div>;
   }
 
   return (
--- front/pages/movies/sunday.js
+++ front/pages/movies/sunday.js
@@ -2,57 +2,37 @@ import React, { useEffect, useState } from 'react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { Box, HStack, Grid, Button, GridItem } from '@chakra-ui/react';
-import useSWR from 'swr';
-import axios from "axios";
 
 import AppLayout from '../../components/AppLayout';
 import ContentsBar from '../../components/ContentsBar';
 import ItemCard from '../../components/ItemCard';
 import WriteButton from '../../components/WriteButton';
 import Pagination from '../../components/Pagination';
-import router from 'next/router';
-
-const useFetch = (category, page) => {
-  const fetcher = (url) => axios.get(url, { withCredentials: true }).then((result) => result.data);
-  const { data, error, mutate } = useSWR(
-    `http://localhost:3060/posts?category=${encodeURIComponent(category)}&page=${page}`, fetcher,
-  );
-  return {
-    data,
-    error,
-    isLoading: !error && !data,
-  }
-}
+import useFetch from '../../hooks/useFetch';
 
 const Sunday = () => {
   const router = useRouter();
+  const category = encodeURIComponent('주일예배')
   const { page } = router.query;
-  const { data, error, isLoading } = useFetch('주일예배', page);
-  const buttonList = ['전체', '성경', '주제'];
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      console.log(data);
-    }, 2000);
-    return () => {
-      clearTimeout(timer);
-    }
-  }, []);
-
+  const url = `/posts?category=${category}&page=${page || 1}`
+  const { data, error, isLoading } = useFetch(url);
+  
   if (isLoading) {
-    <div>로딩 중...</div>
+    return <div>로딩 중...</div>;
   }
   if (error) {
-    <div>게시글을 불러오는 중 문제가 발생했습니다.</div>
+    console.error(error);
+    return <div>게시글을 불러오는 중 문제가 발생했습니다.</div>;
   }
 
+  const buttonList = ['전체', '성경', '주제'];
   return (
     <>
       <Head>
-        <title>예배와 말씀</title>
+        <title>주일예배</title>
       </Head>
       <AppLayout>
-        <ContentsBar title="예배와 말씀" buttonList={buttonList}/>
+        <ContentsBar title="주일예배" buttonList={buttonList}/>
         <Grid templateColumns="repeat(auto-fill, minmax(225px, auto))" gap={5}>
           {data && data.map((post, index) => (
             <ItemCard key={post.content+index} post={post} category={'sunday'}></ItemCard>
