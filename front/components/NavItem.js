e11346a9523d74c796138730db4b7f81100e97a6
Honey <yurjune@naver.com>
2021-10-01 16:17:20 +0900

    Update NavItem: use hover

--- front/components/NavItem.js
+++ front/components/NavItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import Link from 'next/link';
 import { Flex, Box, Button, HStack, useDisclosure } from "@chakra-ui/react";
 import {
@@ -16,24 +16,43 @@ import {
 
 const NavItem = ({ title, items }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const btn = useRef();
+
+  const onRef = (e) => {
+    // console.dir(btn.current);
+    if (!(e.pageY >= btn.current.offsetTop + btn.current.clientHeight)) {
+      return onClose();
+    }
+  };
 
   return (
     <>
       <Menu
-        // isOpen={isOpen}
-        // role="group"
+        isOpen={isOpen}
+        gutter="0"
+        autoSelect="false"
       >
         <MenuButton
           as={Button}
           variant="menu"
-          // _groupHover={onOpen}
-          // onMouseEnter={onOpen}
-          // onMouseLeave={onClose}
+          onMouseEnter={onOpen}
+          onMouseLeave={onClose}
+          ref={btn}
+          // onMouseLeave={onRef}
         >
           {title}
         </MenuButton>
-        <MenuList>
-          {items.map(item => <MenuItem><Link href={item.href}>{item.name}</Link></MenuItem>)}
+        <MenuList
+          width="10px"
+          onMouseEnter={onOpen}
+          onMouseLeave={onClose}
+          transition={{}}
+        >
+          {items.map(item => (
+            <MenuItem _focus={{ bg: "second" }}>
+              <Link href={item.href}>{item.name}</Link>
+            </MenuItem>
+          ))}
         </MenuList>
       </Menu>
     </>
