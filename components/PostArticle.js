ddb8dc9d38b701169ed6037e7cb7729f9d9dd86a
Honey <yurjune@naver.com>
2021-10-15 15:05:42 +0900

    게시글에 이미지 첨부

--- components/PostArticle.js
+++ components/PostArticle.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Box } from '@chakra-ui/react';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Image from 'next/image';
+import { BLOCKS } from '@contentful/rich-text-types';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+
+const renderOption = {
+  renderNode: {
+    [BLOCKS.EMBEDDED_ASSET]: (node, children) => {
+      return (<Image
+        src={`https:${node.data.target.fields.file.url}`}
+        height={node.data.target.fields.file.details.image.height}
+        width={node.data.target.fields.file.details.image.width}
+      />)
+    }
+  }
+}
 
 const PostArticle = ({ article }) => {
+  console.log
   const { paragraph } = article.fields;
   return (
-    <Box>{documentToReactComponents(paragraph)}</Box>
+    <Box>{documentToReactComponents(paragraph, renderOption)}</Box>
   );
 };
 
